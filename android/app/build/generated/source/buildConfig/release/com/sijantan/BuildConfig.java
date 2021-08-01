db4623778c29db868d07bf416fecb48f2674987f
sulthanalihsan <sulthanalihsan5@gmail.com>
2021-08-01 12:36:59 +0800

    tambah animasi auto scroll banner

--- android/app/build/generated/source/buildConfig/release/com/sijantan/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/sijantan/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.sijantan.sijantan;
+package com.sijantan;
 
 public final class BuildConfig {
   public static final boolean DEBUG = false;
-  public static final String APPLICATION_ID = "com.sijantan.sijantan";
+  public static final String APPLICATION_ID = "com.sijantan";
   public static final String BUILD_TYPE = "release";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0.0";
